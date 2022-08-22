1791796cf5055922363e4503758b16d4fbb6f1c9
Rosiyanto Syafrudin <rosiyanto.syafrudin@mekari.com>
2022-08-22 08:39:29 +0700

    add exercise.rb

---
+++ exercise.rb
@@ -0,0 +1,20 @@
+data_user = {
+  "mark" => 1000,
+  "prima" => 2000,
+  "nabil" => 3000,
+  "akbar" => 4000
+}
+def nominal(name, balance)
+  name[balance]
+end
+loop do
+  puts "please enter your username"
+  name = gets.chomp.downcase
+  if data_user.include?(name)
+    puts "#{name} balance is #{nominal(data_user, name)}"
+  else
+    puts "sorry, your input is wrong,
+please enter your name"
+    break
+  end
+end
