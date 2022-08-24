a0ce7e0506df5c4a23fa51f63195e1f82b36f2e2
rosi <rosiyanto.syafrudin@mekari.com>
2022-08-24 14:49:04 +0700

    change parameter.rb

--- parameter.rb
+++ parameter.rb
@@ -4,8 +4,10 @@
 # end
 #
 # hello("assalamu'alaikum wr.wb")
+#hey there, assalamu'alaikum wr.wb
 def hello(halo, calling)
   salam = halo + calling
   puts "hey there, #{salam}"
 end
 hello("assalamu'alaikum wr.wb", " ya ikhwan")
+# => hey there, assalamu'alaikum wr.wb ya ikhwan
